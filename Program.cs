7ddf1559c0b3ed5deff907efcb5307f33610947b
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-10 18:41:28 +0000

    fixed triangele thingsy

--- Program.cs
+++ Program.cs
@@ -244,9 +244,9 @@
 
         else
         {
-            for (int i = 0; i <= n; i++)
+            for (int i = n; i <= 0; i++)
             {
-                for (int i2 = i; i2 < n; i2++)
+                for (int i2 = i; i2 < 0; i2++)
                 {
                     Console.Write("*");
                 }
