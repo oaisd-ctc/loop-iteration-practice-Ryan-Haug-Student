5fa6056f2d5edc76b5e7d3861cbdd5ecb41dc1f0
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-11 16:19:03 +0000

    added outofrange message function and formatted

--- Program.cs
+++ Program.cs
@@ -3,52 +3,71 @@
     public static void Main(string[] args)
     {
         Console.Clear();
+        // Im using console.writeline inbetween all of the funcitions to seperate results and make it easier to read
 
-        // Write1ThroughN_While(-1);
-        // Write1ThroughN_While(5);
-        // Write1ThroughN_While(0);
+        Write1ThroughN_While(-1);
+        Write1ThroughN_While(5);
+        Write1ThroughN_While(0);
 
-        // Write1ThroughN_For(-1);
-        // Write1ThroughN_For(5);
-        // Write1ThroughN_For(0);
+        Console.WriteLine("");
 
-        // WriteNThrough1_While(-1);
-        // WriteNThrough1_While(5);
-        // WriteNThrough1_While(0);
+        Write1ThroughN_For(-1);
+        Write1ThroughN_For(5);
+        Write1ThroughN_For(0);
 
-        // WriteNThrough1_For(-1);
-        // WriteNThrough1_For(5);
-        // WriteNThrough1_For(0);
+        Console.WriteLine("");
 
-        // WriteEvensThrough100();
+        WriteNThrough1_While(-1);
+        WriteNThrough1_While(5);
+        WriteNThrough1_While(0);
 
-        // FindSum(5);
-        // FindSum(1);
-        // FindSum(-3);
-        // FindSum(100);
+        Console.WriteLine("");
 
-        // FindSumOfEvenNumbers(10);
-        // FindSumOfEvenNumbers(-5);
-        // FindSumOfEvenNumbers(2);
-        // FindSumOfEvenNumbers(0);
-        // FindSumOfEvenNumbers(1);
-        // FindSumOfEvenNumbers(25);
-        // FindSumOfEvenNumbers(-25);
+        WriteNThrough1_For(-1);
+        WriteNThrough1_For(5);
+        WriteNThrough1_For(0);
 
-        // FindSumOfOddNumbers(10);
-        // FindSumOfOddNumbers(-5);
-        // FindSumOfOddNumbers(1);
-        // FindSumOfOddNumbers(2);
-        // FindSumOfOddNumbers(25);
-        // FindSumOfOddNumbers(-25);
+        Console.WriteLine("");
+
+        WriteEvensThrough100();
+
+        Console.WriteLine("");
+
+        FindSum(5);
+        FindSum(1);
+        FindSum(-3);
+        FindSum(100);
+
+        Console.WriteLine("");
+
+        FindSumOfEvenNumbers(10);
+        FindSumOfEvenNumbers(-5);
+        FindSumOfEvenNumbers(2);
+        FindSumOfEvenNumbers(0);
+        FindSumOfEvenNumbers(1);
+        FindSumOfEvenNumbers(25);
+        FindSumOfEvenNumbers(-25);
+
+        Console.WriteLine("");
+
+        FindSumOfOddNumbers(10);
+        FindSumOfOddNumbers(-5);
+        FindSumOfOddNumbers(1);
+        FindSumOfOddNumbers(2);
+        FindSumOfOddNumbers(25);
+        FindSumOfOddNumbers(-25);
 
         OutputRightTriangle(4);
         OutputRightTriangle(6);
+        Console.WriteLine("");
         OutputRightTriangle(-4);
         OutputRightTriangle(-6);
         OutputRightTriangle(0);
     }
-
+    public static void OutOfRangeMessage(int n)
+    {
+        Console.WriteLine(n + " Is out of range.");
+    }
     public static void Write1ThroughN_While(int n)
     {
         int i = 0;
@@ -63,7 +82,7 @@
         }
         else
         {
-            Console.WriteLine(n + " is out of range");
+            OutOfRangeMessage(n);
         }
     }
 
@@ -78,7 +97,7 @@
         }
         else
         {
-            Console.WriteLine(n + " is not in range.");
+            OutOfRangeMessage(n);
         }
     }
 
@@ -94,7 +113,7 @@
         }
         else
         {
-            Console.WriteLine(n + " is out of range");
+            OutOfRangeMessage(n);
         }
     }
 
@@ -110,7 +129,7 @@
 
         else
         {
-            Console.WriteLine(n + " Is out of range.");
+            OutOfRangeMessage(n);
         }
     }
 
@@ -240,6 +259,11 @@
                 }
                 Console.WriteLine("");
             }
+        }
+
+        else if (n == 0)
+        {
+            OutOfRangeMessage(n);
         }
 
         else
